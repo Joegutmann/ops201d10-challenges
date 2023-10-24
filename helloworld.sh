c75fbec5eeb9e83920ba931e3d4bdd9f85337fb5
JoeGutmann <Joe.Gutmann@gmail.com>
2023-10-24 18:41:39 -0500

    first challenge attempt

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Joe Gutmann
+# Date of latest revision:      24Oct2023
+# Purpose:                      Ops Challenge: Class 02
+
+# Declaration of variables
+variable1="Hello World!"
+variable2="I am working perfectly!"
+
+# Declaration of functions
+
+
+# Main
+echo "Hello World!" #for assignment
+echo $variable1 # trying to learn how to use variables rather than just printing
+echo $variable2
