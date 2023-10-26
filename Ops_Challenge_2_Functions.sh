a53b214da43ae9fb5f1a93e480fb22c0a30b05a4
JoeGutmann <Joe.Gutmann@gmail.com>
2023-10-25 19:13:52 -0500

    My First Function

---
+++ Ops_Challenge_2_Functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge - Functions
+# Author:                       Joe Gutmann
+# Date of latest revision:      25Oct2023
+# Purpose:                      Ops Challenge: Class 03
+
+# Declaration of variables
+#login="This is the login history"
+
+
+
+# Declaration of functions
+print_login() {
+last
+echo "This is the login history"
+}
+
+# Main
+print_login
