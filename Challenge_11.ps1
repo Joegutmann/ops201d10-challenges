4af5161ed2d0e6e1ba162a3e213054f0c8fab12d
Joe Gutmann <147211944+Joegutmann@users.noreply.github.com>
2023-11-06 17:36:02 -0800

    Add files via upload

---
+++ Challenge_11.ps1
@@ -0,0 +1,14 @@
+﻿# Enable FIle Printer sharing
+
+Set-NetFirewallRule -displaygroup "File and printer sharing" -Enabled True
+
+ # Allow icmp traffic
+
+ # Enable remote management
+
+ # remove bloatware
+
+
+ # enable hpyer v
+
+# disable smbv1 insecure protocol
